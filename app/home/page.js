6b9eee77c5ed6e0e2bdfb1ee4462a4fd8d3c5868
Austin-seth <183656254+Austin-seth@users.noreply.github.com>
2025-05-08 05:51:45 -0700

    update

--- app/home/page.js
+++ app/home/page.js
@@ -10,7 +10,7 @@ import ContactUs from "../component/home/ContactUs";
 import FAQSection from "../component/home/FAQs";
 import Footer from "../component/home/Footer";
 
-const Page = () => {
+const Home = () => {
   return (
     <div className="w-full  bg-[#f3fff8]">
       <Hero />
@@ -26,4 +26,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default Home;
